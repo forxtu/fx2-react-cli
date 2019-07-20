3f89963416bd33ac5ae85d410fb8ff05af3cc37f
forxtu <forxtu@gmail.com>
2019-07-20 20:18:31 +0200

    feat(createFeature): basic functionality

--- index.js
+++ index.js
@@ -4,18 +4,26 @@ let program = require("commander");
 // modules
 const createComponent = require("./modules/createComponent");
 const createHook = require("./modules/createHook");
+const createFeature = require("./modules/createFeature");
 
 program
   .command("gc <component>")
   .option("-c, --classComponent", "Create class component")
   .option("-t, --typescript", "Create typescript file")
   .option("-n, --nofolder", "Do not wrap component in folder")
+  .option("-g, --global", "Create component in 'src/components' folder")
   .action(createComponent);
 
 program
-  .command("gh <component>")
+  .command("gh <hook>")
   .option("-t, --typescript", "Create typescript file")
   .option("-n, --nofolder", "Do not wrap hook in folder")
   .action(createHook);
+
+program
+  .command("gf <feature>")
+  .option("-t, --typescript", "Create typescript file")
+  .option("-g, --global", "Create feature in 'src/features' folder")
+  .action(createFeature);
 
 program.parse(process.argv);
---
+++ modules/createFeature.js
@@ -0,0 +1,121 @@
+const fs = require("fs-extra");
+const replace = require("replace");
+
+// utils
+const { capitalize } = require("../utils/common");
+const {
+  getNoFolderPath,
+  getElementName,
+  getGlobalPath
+} = require("../utils/selectors");
+
+// templates
+const templates = require("../templates/templates");
+
+let newFeaturePath;
+let global;
+let typescript;
+
+module.exports = async function createFeature(feature, cmd) {
+  newFeaturePath = feature;
+
+  cmd.global ? (global = true) : (global = false);
+  cmd.typescript ? (typescript = true) : (typescript = false);
+
+  // Global path
+  const globalDir = "./src/features";
+
+  if (global) {
+    newFeaturePath = await getGlobalPath(globalDir, feature);
+  }
+
+  generateBoilerplate(newFeaturePath);
+};
+
+function generateBoilerplate(path) {
+  const featureName = getElementName(path);
+
+  if (!fs.existsSync(path)) {
+    processFeatureElements(path, featureName);
+
+    console.log(`Feature "${featureName}" created at "${path}"`.cyan);
+  } else {
+    console.log(
+      `Feature "${featureName}" already exists at "${path}", choose another name if you want to create a new feature`
+        .red
+    );
+  }
+}
+
+function processFeatureElements(path, featureName) {
+  const featureElements = [
+    { type: "components", name: "FeatureComponent", suffix: "" },
+    { type: "containers", name: "FeatureContainer", suffix: "Container" },
+    { type: "actions", name: "FeatureActions", suffix: "Actions" },
+    { type: "reducers", name: "FeatureReducer", suffix: "Reducer" }
+  ];
+
+  const capitalizedFeatureName = capitalize(featureName);
+
+  const getTypescriptExtenstion = () => (typescript ? "ts" : "js");
+  const getTypescriptExtenstionTsx = () => (typescript ? "tsx" : "js");
+  const getTypescriptExtenstionWithType = element => {
+    return typescript
+      ? element.type === "components" || element.type === "containers"
+        ? "tsx"
+        : "ts"
+      : "js";
+  };
+
+  const getDefaultPath = element => {
+    return `${path}/${element.type}/${
+      element.name
+    }.${getTypescriptExtenstionWithType(element)}`;
+  };
+
+  // Step 1 - copy files from templates
+  fs.copySync(
+    `${require("path").dirname(
+      require.main.filename
+    )}/templates/feature/${getTypescriptExtenstion()}`,
+    `${path}`
+  );
+
+  const renameIfPathExists = process => {
+    fs.pathExists(
+      `${path}/components/FeatureComponent.${getTypescriptExtenstionTsx()}`,
+      (err, exists) => {
+        if (exists) {
+          process();
+        }
+        if (err) {
+          console.log(`Something went wrong ${err}`.red);
+        }
+      }
+    );
+  };
+
+  const processData = () => {
+    featureElements.forEach(element => {
+      fs.renameSync(
+        getDefaultPath(element),
+        `${path}/${element.type}/${capitalizedFeatureName}${capitalize(
+          element.suffix
+        )}.${getTypescriptExtenstionWithType(element)}`,
+        err => {
+          console.log(`Something went wrong ${err}`.red);
+        }
+      );
+    });
+    replace({
+      regex: ":name",
+      replacement: `${path}`,
+      paths: [capitalizedFeatureName],
+      recursive: true,
+      silent: true
+    });
+  };
+
+  // Step 2 - rename files and replace :name
+  renameIfPathExists(processData);
+}
