caae973d06c879fe936db1c574a12b20eea7f215
forxtu <forxtu@gmail.com>
2019-07-23 17:07:13 +0200

    feat(index): description to the commands

--- index.js
+++ index.js
@@ -7,23 +7,23 @@ const createHook = require("./modules/createHook");
 const createFeature = require("./modules/createFeature");
 
 program
-  .command("gc <component>")
+  .command("gc <component>", "Generate component")
   .option("-c, --classComponent", "Create class component")
-  .option("-t, --typescript", "Create typescript file")
+  .option("-t, --typescript", "Create typescript component file")
   .option("-n, --nofolder", "Do not wrap component in folder")
   .option("-g, --global", "Create component in 'src/components' folder")
   .action(createComponent);
 
 program
-  .command("gh <hook>")
-  .option("-t, --typescript", "Create typescript file")
+  .command("gh <hook>", "Generate hook")
+  .option("-t, --typescript", "Create typescript hook file")
   .option("-n, --nofolder", "Do not wrap hook in folder")
   .option("-g, --global", "Create hook in 'src/hooks' folder")
   .action(createHook);
 
 program
-  .command("gf <feature>")
-  .option("-t, --typescript", "Create typescript file")
+  .command("gf <feature>", "Generate feature with react-redux")
+  .option("-t, --typescript", "Create typescript feature files")
   .option("-g, --global", "Create feature in 'src/features' folder")
   .action(createFeature);
 
